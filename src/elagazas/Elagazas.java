d5060388176740ed3205d47e4d5504f1db1b4ee8
pettendililla <pettendililla@gmail.com>
2025-02-17 13:10:26 +0100

    Összehasonlító műveletek

--- src/elagazas/Elagazas.java
+++ src/elagazas/Elagazas.java
@@ -14,7 +14,34 @@ public class Elagazas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int szam = 5;
+        
+        if (szam >= 0){
+            System.out.println("A szám pozitív.");
+        }
+        else{
+            System.out.println("A szám negatív");
+        }
+        boolean feltetel = (szam >= 0);
+        if (feltetel){
+            System.out.println("A szám pozitív");
+        }
+        else{
+            System.out.println("A szám pozitív");
+        }
+        
+        System.out.println("Kérek egy számot");
+        Scanner be = new Scanner(System.in);
+        int a = be.nextInt();
+        System.out.println("Kérek egy másik számot");
+        int b = be.nextInt();
+        if(a < b){
+            System.out.printf("A %d nagyobb, mint %d", b,a);
+        }
+        else{
+            System.out.printf("A %d nagyobb, mint %d",a ,b);
+            
+        }
     }
     
 }
